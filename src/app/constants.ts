fee9c35631b30594b150d4aee01cbf2f49668f67
GibbyH <gibbidy4213z@gmail.com>
2019-11-11 16:16:30 -0800

    added validators to the product add component and needed fields in the HTML. Error message only appearing on name field, will address later

---
+++ src/app/constants.ts
@@ -0,0 +1,6 @@
+export const PRODUCT_CONSTANTS = {
+    productAddName: 'Product Name',
+    productAddDescription: 'Product Description',
+    productAddPrice: 'Product Price',
+    productAddCreate: 'Create Product'
+}
